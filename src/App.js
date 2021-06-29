51225156ab5627d4d6994575d7c8a63bca43229c
Ibrokhim41 <ibrokhim.abdurakhmonov@gmail.com>
2021-06-29 18:42:56 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 // import Example from "./components/Example"
 import Footer from "components/Footer";
 import Header from "components/Header";
-import Discounts from "pages/Discounts";
-
+import Home from "pages/Home";
 
 function App() {
     return (
         <div className="App mb-20 sm:mb-0">
             <Header />
-<<<<<<< HEAD
-            <Discounts />
-=======
             <Home />
-            {/* <Discounts /> */}
->>>>>>> f0869490281bd9b64d35b9da05c65b5414f89d3a
             <Footer />
         </div>
     );
