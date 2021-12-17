ee8a7c1de7d73734305b5f8565b64b0c1f833814
Ibrokhim41 <ibrokhim.abdurakhmonov@gmail.com>
2021-12-17 16:49:53 +0500

    fix

--- src/components/Modals/RegionChoose.jsx
+++ src/components/Modals/RegionChoose.jsx
@@ -61,8 +61,8 @@ const RegionChoose = () => {
     return (
         <div 
             // ${regionChoose ? 'translate-y-0' : 'translate-y-full'}
-            style={{translateY: regionChoose ? '0' : '1000px'}}
-            className={`fixed top-0  transform transition-all w-full h-full bg-white sm:bg-black-black sm:bg-opacity-30 flex justify-center items-center z-100`}>
+            style={{ transform: `translateY(${regionChoose} ? '0' : '1000px')`}}
+            className={`fixed top-0 transition-all w-full h-full bg-white sm:bg-black-black sm:bg-opacity-30 flex justify-center items-center z-100`}>
             <AiOutlineClose
                 onClick={() => dispatch(setRegionChoose(false))}
                 className="absolute top-5 right-5 cursor-pointer text-2xl text-white sm:text-3xl lg:text-4xl hover:text-red" />
